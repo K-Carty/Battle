ad0cd1e4eceee9fa176a47e24d3aef1319288301
Kieran Carty <kierancarty@Kierans-MacBook-Air.local>
2022-05-06 17:43:03 +0100

    half way through Entering Players

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -5,9 +5,12 @@ require_relative "../app"
 
 Capybara.app = Battle
 
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+feature 'Fills in player names' do
+  scenario 'players can fill in their names, submit a form, and see those names on-screen' do
     visit("/")
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
+    fill_in :name1 , with: "Gawain"
+    fill_in :name2 , with: "Kieran"
+    click_button "Submit"
+    expect(page).to have_content "Player 1: #{name1} ready! vs Player 2: #{name2} ready!"
+  end 
+end 
